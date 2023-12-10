82a5528e0bab9459a0a7be0de8296427f98f02e0
Micah Ives <micbenives@gmail.com>
2023-12-10 12:14:33 -0600

    Update server, add home-routes and utils/helpers for handlebars

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const path = require('path');
 require('dotenv').config();
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
-const exp = require('constants');
+
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -23,8 +24,26 @@ app.get('/partials', (req, res) => {
     // serves the body of the page "main.hbs" to the container, "index.hbs"
     res.render('main', { layout : 'index' });
 });
-    
 
-app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}!`);
-})
+const sess = {
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+        // stored in milliseconds
+        maxAge: 24 * 60 * 60 * 1000, // expires after one day
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize,
+    }),
+};
+
+app.use(session(sess));
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () =>
+      console.log(
+        `\nServer running on port ${PORT}. Visit http://localhost:${PORT} and create an account!`
+      )
+    );
+  });
