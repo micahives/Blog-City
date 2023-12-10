d8bc8eaa54afc20a5e9ee3f253e15246892aeb90
Micah Ives <micbenives@gmail.com>
2023-12-10 11:42:06 -0600

    Add user route, update file structure

--- server.js
+++ server.js
@@ -1,25 +1,23 @@
 const express = require('express');
-const handlebars = require('express-handlebars');
+const exphbs = require('express-handlebars');
 const path = require('path');
-require('dotenv').config()
+require('dotenv').config();
+const hbs = exphbs.create({});
 
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const exp = require('constants');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
+// set handlebars as default template engine "turn on handlebars"
+app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
-
-// sets handlebars configurations 
-app.engine('hbs', handlebars({
-    layoutsDir: __dirname + '/views/layouts',
-    extname: 'hbs'
-}));
 
 app.get('/', (req, res) => {
     // serves the body of the page "main.handlebars" to the container, "index.handlebars"
