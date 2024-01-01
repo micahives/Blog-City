6f4901c8a418d480026d5e492aba5a1e46e51f57
Micah Ives <micbenives@gmail.com>
2023-12-31 20:20:18 -0600

    Add delete route and functionality for posts

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -20,6 +20,7 @@ router.get('/', async (req, res) => {
     }
   });
 
+  // create a new blog post
 router.get('/new-post', (req, res) => {
     console.log('New Post route hit!');
     const username = req.session.username;
@@ -46,5 +47,42 @@ router.get('/new-post', (req, res) => {
       res.status(500).json({ message: 'Internal Server Error' });
     }
   });
+
+router.get('/blog-post/:id', async (req, res) => {
+    try {
+        const blogPostId = req.params.id;
+        const blogPost = await BlogPost.findByPk(blogPostId);
+
+        if (!blogPost) {
+            res.status(404).render('404'); // should create a 404 handlebar template
+            return;
+        }
+
+        res.render('blog-post-detail', { blogPost });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json(err);
+    }
+});
+
+// DELETE route for deleting a blog post
+router.delete('/blog-post/:id', async (req, res) => {
+  const postId = req.params.id;
+
+  try {
+    const blogPost = await BlogPost.findByPk(postId);
+
+    if (!blogPost) {
+      return res.status(404).json({ error: 'Blog post not found' });
+    }
+
+    await blogPost.destroy();
+
+    res.json({ message: 'Blog post deleted successfully' });
+  } catch (error) {
+    console.error('Error deleting blog post:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
   module.exports = router;
